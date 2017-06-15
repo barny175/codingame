aa347d9cfee6609ebeaff1f9935acc7f893539d3
Barny <barny175@gmail.com>
2017-06-15 21:55:51 +0200

    + return sum of taken groups
    + function ride2 using function splitGroups

--- Puzzles/RollerCoaster/src/Lib.hs
+++ Puzzles/RollerCoaster/src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (selectGroups, ride, splitGroups) where
+module Lib (selectGroups, ride, splitGroups, ride2) where
 
 import Control.Monad
 import Control.Monad.State
@@ -12,11 +12,21 @@ selectGroups groups@(g:gs) places
     | g <= places = (g:sg, rest)
     where (sg,rest) = selectGroups gs (places - g)
 
-splitGroups :: Array Int Int -> Int -> Int -> Int
+splitGroups :: Array Int Int -> Int -> Int -> (Int, Int)
 splitGroups ga start places =
     let waitingGroupCount = min (places - start) ((snd $ bounds ga) - start)
         sumA n = sum $ map (\i -> ga ! i) (take n [start..])
-    in length . filter (<=places) $ map sumA [1..waitingGroupCount]
+        sums = map sumA [1..waitingGroupCount]
+        taken = length $ filter (<=places) sums
+    in (taken, if taken > 0 then sums !! (taken - 1) else 0)
+--
+ride2 places rides groups start people
+    | rides == 0 = 0
+    | people < places = rides * people
+    | otherwise = dirhams
+    where (takenGroups, sum') = splitGroups groups start places
+          rest = (start + takenGroups) `mod` places
+          dirhams = sum' + (ride2 places (rides - 1) groups rest people)
 
 ride places rides groups people
     | rides == 0 = 0
