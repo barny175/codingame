99ed8243b424f20c4096932cbdab994b8ad1dd4b
Barny <barny175@gmail.com>
2017-12-16 16:16:49 +0100

    test for more tunnel with more possible paths

--- JavaPuzzles/test/lastcrusade/PlayerTest.java
+++ JavaPuzzles/test/lastcrusade/PlayerTest.java
@@ -175,4 +175,16 @@ public class PlayerTest {
                 .extracting(Player.Command::toString)
                 .contains("1 2 RIGHT", "1 2 RIGHT");
     }
+
+    @Test
+    public void findPath_more_paths() {
+        ArrayList<List<Integer>> tunnel = new ArrayList<>();
+        tunnel.add(Lists.newArrayList(0, -3, 0, 0));
+        tunnel.add(Lists.newArrayList(12, 5, 13, 0));
+        tunnel.add(Lists.newArrayList(11, 12, 9, 0));
+        tunnel.add(Lists.newArrayList(0, 0, -3, 0));
+        Player player = new Player(4, 4, tunnel, 2);
+        List<Player.Path> paths = player.findPaths(1, 0, Player.Dir.TOP);
+        assertThat(paths).hasSize(2);
+    }
 }
